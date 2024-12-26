2bca5f14ebb7888aac5f85ab4c81942ee9815fe4
eyoas mulat <desalegnmulat5@gmail.com>
2024-12-26 10:04:58 -0800

    the signup and login page

---
+++ 1-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import LoginSignup from './componenet/LoginSignup';
+
+function App() {
+  return (
+    <div className="App">
+     <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
---
+++ 1-app/src/componenet/LoginSignup.jsx
@@ -0,0 +1,40 @@
+import React,{useState } from "react";
+import './LoginSignup.css'
+// import email_icon from './email_icon.svg'; 
+// import password_icon from './password_icon.svg';
+// import user_icon from './user_icon.svg';
+
+function LoginSignup() {
+    const [action ,setAction] =useState("Login")
+    return ( 
+   <div className="container">
+     <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+     </div>
+     <div className="inputs">
+        {action==="Login"? <div></div>:<div className="input"><img src="" alt=""/>
+        <input placeholder="name" type="text"></input></div>}
+     
+     <div className="input">
+     <img src="" alt="" />
+     <input type="email" placeholder="email"/>
+     </div>
+     <div className="input">
+     <img src="" aslt="" />
+     <input type="password" placeholder="password"/>
+     </div>
+     </div>
+     {action==="Sign up"? <div></div>: <div className="forgot-password">lost password?<span>click here!</span></div>}
+    
+     <div className="submit-container">
+     <div className= {action==="Login"?"submit gray":"submit" } onClick={()=>{setAction("Sign up")}}>
+            Sign up
+        </div>
+        <div className={action==="Sign up"? "submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+     </div>
+   </div>
+     );
+}
+
+export default LoginSignup;
